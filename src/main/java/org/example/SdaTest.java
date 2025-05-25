0147d567ff637942d974b1515d74d697e8f4cc4c
HansSima <SimaHonza@seznam.cz>
2025-05-25 13:26:39 +0200

    new

--- src/main/java/org/example/SdaTest.java
+++ src/main/java/org/example/SdaTest.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
 public class SdaTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+
+    }
+
 
 }
