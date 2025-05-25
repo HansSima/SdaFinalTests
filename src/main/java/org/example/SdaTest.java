f9507c7c706bcd7b739742bb7a89e5d65a98b339
HansSima <SimaHonza@seznam.cz>
2025-05-25 13:59:33 +0200

    test

--- src/main/java/org/example/SdaTest.java
+++ src/main/java/org/example/SdaTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,9 +21,12 @@ public class SdaTest {
     public void sdaTest() {
         //Toto mi na jede na stranku tutorialpoint
         driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+        //Toto mi zvetsi obrazovku
+        driver.manage().window().setSize(new Dimension(1936, 1048));
         //Toto mi najde na strance prvek a vepise jmeno
         driver.findElement(By.id("name")).sendKeys("Jenda");
-
+        //Toto mi najde na strance prvek a vepise mail
+        driver.findElement(By.id("email")).sendKeys("email@email.cz");
     }
 
 
